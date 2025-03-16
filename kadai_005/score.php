ef2ead5f8ebf1e0d91c3416e5d765fb6b338ada3
tecotoco <mai_n_0722@yahoo.co.jp>
2025-03-16 20:53:04 +0900

    第5章課題をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,17 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$score11 = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 +$score7 + $score8 + $score9 + $score10;
+echo $score11;
+echo '<br>';
+echo $score11 / 10;
+?>
